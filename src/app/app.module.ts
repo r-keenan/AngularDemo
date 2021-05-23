03fe99acf1dbfd3d7903094625f1e713f9c14c2b
Ross Keenan <ross.keenan@protonmail.com>
2021-05-23 07:54:07 -0500

    React is so much better than Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ServerComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+//selector is a our own custom HTML tag. in this case, it is <app-server></app-server>
+
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
